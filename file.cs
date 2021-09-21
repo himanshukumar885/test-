dc1a9bbd49d6dfc0d5ec63a2be53bb2d73acf41f
kavitakushawaha <kavitakushawaha@bidgely.com>
2021-09-22 01:34:36 +0530

    added user input

--- file.cs
+++ file.cs
@@ -4,5 +4,10 @@ public class a
 public static void main(String args[])
 {
 Console.WriteLine("Hello ");
+string s = COnsole.ReadLine("Are you hungry");
+if(s==yes)
+Console.WriteLine("yes, i am hungry");
+else
+Console.WriteLine("No, i m not hungry");
 }
 }
